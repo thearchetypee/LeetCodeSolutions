4519af9b740360a91375cceefe80154aadf4aefb
Aayush Chaudhary <aayushchaudhary287@gmail.com>
2024-01-04 12:10:41 +0530

    Gas Station - Approach2 Optimised the solution to run in O(n)

--- src/January/GasStation.kt
+++ src/January/GasStation.kt
@@ -1,6 +1,32 @@
 package January
 
 
+fun canCompleteCircuit(gas: IntArray, cost: IntArray): Int {
+    val n = gas.size
+    var currGas = 0 // Current gas at the current station
+    var totalGas = 0 // Total gas surplus/deficit over the entire circuit
+    var startIndex = 0 // Index of the starting station
+
+    for (i in 0 until n) {
+        val diff = gas[i] - cost[i] // Calculate the gas difference at the current station
+        currGas += diff // Add the difference to the current gas
+
+        totalGas += diff // Update the total gas surplus/deficit
+
+        if (currGas < 0) {
+            // If we run out of gas at the current station, reset the starting station
+            currGas = 0
+            startIndex = i + 1
+        }
+    }
+
+    if (totalGas < 0) {
+        // If the total gas surplus/deficit is negative, it's not possible to complete the circuit
+        return -1
+    }
+
+    return startIndex // Return the index of the valid starting station
+}
 
 fun canCompleteCircuitApproach1(gas: IntArray, cost: IntArray): Int {
     val n = gas.size
