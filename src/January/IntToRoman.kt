5d1d3d87d81d35cb7ffec81b127b0c46e34bd95d
aayush287 <aayushchaudhary287@gmail.com>
2024-01-12 19:45:33 +0530

    12. Integer to Roman

---
+++ src/January/IntToRoman.kt
@@ -0,0 +1,24 @@
+package January
+
+import java.lang.StringBuilder
+
+fun intToRoman(num: Int): String {
+    val values = intArrayOf(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+    val symbols = arrayOf("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I")
+
+    val result = StringBuilder()
+    var n = num
+
+    for (i in values.indices) {
+        while (n >= values[i]) {
+            result.append(symbols[i])
+            n -= values[i]
+        }
+    }
+
+    return result.toString()
+}
+
+fun main() {
+    println(intToRoman(5532))
+}
