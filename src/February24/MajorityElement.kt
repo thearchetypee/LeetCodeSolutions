4bcb2d08dd81953689f6d5b6dc98eb83b8983779
aayush287 <aayushchaudhary287@gmail.com>
2024-02-12 09:50:04 +0530

    Majority element Implmented using Boyer-Moore's Algorithm

--- src/February24/MajorityElement.kt
+++ src/February24/MajorityElement.kt
@@ -1,15 +1,56 @@
 package February24
 
+/**
+ * Approach 2: Boyer-Moore's Voting algorithm
+ *
+ *  This algorithms finds the majority element, if one exists.
+ *  The majority element in an array is an element that occurs more than half of the size of array.
+ *
+ * **Note:** This algorithm only return majority element if it exists. If there is no majority element,
+ *      then this will return arbitrary element. We can make some minor improvement in the algorithm to
+ *      return -1 or null if majority element doesn’t exist.
+ *
+ * ### Algorithm
+ *
+ * 1. Initialise two variables: `num` and `count` .
+ *    `num` will hold the majority element, and `count` will determine if `num` is majority element or not.
+ * 2. Iterate through the elements of array
+ * 3. If `count` is `0` then set `num` to current element.
+ * 4. If current element is equal to `num`, then increment `count`
+ * 5. If current element is not equal to `num`,then decrement `count`
+ * 6. Return `num` after traversing array
+ */
+
+fun majorityElement(nums: IntArray): Int {
+    val n = nums.size
+    var count = 0
+    var num = -1
+    for (i in 0..<n) {
+        if (count == 0) {
+            num = nums[i]
+        }
+        if(num == nums[i]) {
+            count++
+        } else {
+            count--
+        }
+    }
+    return num
+}
 
 /**
- * Approach 1
+ * Approach 1: HashMap
  *
  * To get the majority element we store the number of counts of distinct elements
  * in a map. After we get the count of all distinct element then we iterate over map
  * and check for each distinct element is greater than (size of array)/2.
  * Return once this condition is met.
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ *
  */
-fun majorityElement(nums: IntArray): Int {
+fun majorityElementApproach1(nums: IntArray): Int {
     val map = mutableMapOf<Int, Int>()
     val n = nums.size
     for(i in 0..<n) {
